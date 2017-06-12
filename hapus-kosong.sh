e86c0459275f9683e184fc39daa5199f697e839c
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2017-06-12 12:42:43 +0700

    hapus berkas paket zero byte

---
+++ hapus-kosong.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+repo_dir="$HOME/mirror"
+repo_dists=$1
+repo_sect=$2
+repo_arch=$3
+repo_pkglist="$repo_dir/dists/$repo_dists/$repo_sect/binary-$repo_arch/Packages"
+
+if [ ! -f "$repo_pkglist" ]
+then
+ echo "Error:"
+ echo "- $repo_pkglist"
+ echo "File not exists."
+fi
+
+for i in `cat $repo_pkglist | grep Filename: | awk '{print $2}'`
+do
+ if [ ! -s "$repo_dir/$i" ]
+ then
+  echo $repo_dir/$i
+  rm $repo_dir/$i
+ fi
+done
+
